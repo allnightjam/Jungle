c42511a88c970c69af0d081a34c60f634f3d0077
Joyce Ann Miguel <joyce.a.miguel@gmail.com>
2023-07-03 08:45:57 -0400

    changing keys

---
+++ spec/product_spec.rb
@@ -0,0 +1,8 @@
+RSpec.describe Product, type: :model do
+  describe 'Validations' do
+    validates :name, presence: true
+    validates :price, presence: true
+    validates :quantity, presence: true
+    validates :category, presence: true
+  end
+end
